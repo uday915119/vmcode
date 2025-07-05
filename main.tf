a29c0e0b1676161b84f497036cc1fe36c8d1a431
Uday Singh <pruday32@gmail.com>
2025-07-05 21:32:18 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,122 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    depends_on          = [azurerm_resource_group.rg]
+  name                = var.vnet_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]      # ✅ ye hona zaroori hai
+
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = var.subnet1
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.10.0/24"] // Changed range
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = var.subnet2
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.20.0/24"] // Changed range
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1010
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["80"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet1_nsg" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = var.public_ip_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = var.vm
+  resource_group_name             = var.rg
+  location                        = var.location
+  size                            = "Standard_F2"
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+data "azurerm_public_ip" "assigned" {
+  name                = azurerm_public_ip.public_ip.name
+  resource_group_name = azurerm_resource_group.rg.name
+  depends_on          = [azurerm_linux_virtual_machine.vm]
+}
+
+output "vm_public_ip" {
+  value       = data.azurerm_public_ip.assigned.ip_address
+  description = "Public IP of the VM after allocation"
+}
+
+resource "azurerm_virtual_machine_extension" "nginx" {
+  name                 = "nginx-install"
+  virtual_machine_id   = azurerm_linux_virtual_machine.vm.id # replace with your actual VM resource name
+  publisher           = "Microsoft.Azure.Extensions"
+  type                = "CustomScript"
+  type_handler_version = "2.1"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "sudo apt-get update && sudo apt-get install -y nginx"
+    }
+SETTINGS
+}
+
+
